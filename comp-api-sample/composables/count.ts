02a76a79426257bbf3a62e5bf34b43c714a6f421
yuki <59268274+yuki-skmt@users.noreply.github.com>
2022-07-28 21:00:37 +0900

    first commit

---
+++ comp-api-sample/composables/count.ts
@@ -0,0 +1,13 @@
+export const useCounter = () => {
+  const count = ref(0);
+  const double = computed(() => count.value * 2);
+  const increment = () => count.value++;
+  const decrement = () => count.value--;
+
+  return {
+    count,
+    double,
+    increment,
+    decrement,
+  };
+};
---
+++ comp-api-sample/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useCounter } from "~~/composables/count";
+const { count, double, increment, decrement } = useCounter();
+</script>
+
+<template>
+  <h1>カウントするページ</h1>
+  <p>カウント：{{ count }}</p>
+  <p>カウント（2倍）：{{ double }}</p>
+  <button @click="decrement">減らす</button>
+  <button @click="increment">増やす</button>
+</template>
